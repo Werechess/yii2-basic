9dfef898e8dc0ef21a70dd9c430f1966b50b068d
Werechess <werechess@gmail.com>
2016-03-06 22:44:58 +0300

    Order table model and GRUD added

---
+++ models/Order.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_order".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $address
+ * @property string $email
+ * @property string $phone
+ * @property string $date_create
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_order';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'address', 'email', 'phone', 'date_create'], 'required'],
+            [['date_create'], 'safe'],
+            [['name', 'address', 'email'], 'string', 'max' => 255],
+            [['phone'], 'string', 'max' => 10],
+            [['email'], 'unique'],
+            [['phone'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'address' => Yii::t('app', 'Address'),
+            'email' => Yii::t('app', 'Email'),
+            'phone' => Yii::t('app', 'Phone'),
+            'date_create' => Yii::t('app', 'Date Create'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\query\OrderQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \app\models\query\OrderQuery(get_called_class());
+    }
+}
